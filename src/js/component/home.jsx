64d87a6ea6d0e49d51fff14f43a10e36c8007c20
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-11-13 20:51:19 +0000

    Se agregan tareas y se pueden eliminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Swal from 'sweetalert2'
 
-//create your first component
+const API_BASE_URL = "https://playground.4geeks.com/todo/";
+
+
 const Home = () => {
 
 	const [inputValue, setInputValue] = useState("")
 	const [listTask, setListTask] = useState([])
 
+
+	// Funcion para mostrar la lista de todos
 	const getUserTodosList = async () => {
 		try {
-			const response = await fetch(`https://playground.4geeks.com/todo/users/Felipe`);
+			const response = await fetch(`${API_BASE_URL}users/Felipe`);
 			const result = await response.json()
 			setListTask(result.todos)
 		} catch (error) {
@@ -18,11 +23,67 @@ const Home = () => {
 
 	useEffect(() => {
 		getUserTodosList();
-
 	}, [])
 
-	console.log(listTask);
 
+	// Funcion para guardar todos
+	const saveToDo = async () => {
+		if (inputValue) { // Se agrega este if para no tener tareas sin nada escrito
+
+			try {
+				const body = {
+					"label": inputValue,
+					"is_done": false
+				}
+
+				const response = await fetch(`${API_BASE_URL}todos/Felipe`, {
+					method: "POST",
+					headers: {
+						accept: 'application/json',
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(body)
+				})
+
+				if (response.ok) {
+					alert('Se agregó la tarea exitosamente!')
+				}
+
+				const result = await response.json()
+				getUserTodosList(prevState => [...prevState, result])
+				setInputValue("")
+
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	}
+
+
+	// Function para borrar una tarea a traves del index
+	const deleteTask = async (taskId) => {
+
+		try {
+			const response = await fetch(`${API_BASE_URL}todos/${taskId}`, {
+				method: "DELETE",
+			});
+	
+			if (response.ok) {
+				// Actualiza la lista de tareas en el estado
+				setListTask(prevTasks => prevTasks.filter(task => task.id !== taskId));
+				alert("Tarea eliminada exitosamente!")
+			}
+					
+		} catch (error) {
+			console.error("Error borrando task:", error);
+		}
+	};
+	// Function para limpiar todas las tareas
+	const deleteAllTasks = async () => {
+		tasks.forEach((task) => {
+			deleteTask(task.id)
+		})
+	};
 
 
 	return (
@@ -36,15 +97,21 @@ const Home = () => {
 						onChange={(event) => setInputValue(event.target.value)}
 						type="text" className="form-control"
 						placeholder="Ingrese una tarea..." />
+					<button type="button" className="btn btn-outline-success ms-2" onClick={saveToDo}>Añadir tarea</button>
+					<button type="button" className="btn btn-outline-danger ms-2" onClick={deleteAllTasks}>Vaciar lista</button>
 				</div>
 			</div>
 
 			<div className="container d-flex justify-content-center bg-light p-3 rounded my-2" style={{ width: "40rem" }}>
-				<ul>
+				<ul className="list-group list-group-flush" style={{ width: '100%' }}>
 					{listTask.map(task => (
-						<div key={task.id}>
-							<li>{task.label}</li>
-						</div>
+						<li key={task.id} className="list-group-item bg-light d-flex justify-content-between align-items-center">
+							{task.label}
+							<div>
+								<button type="button" className="btn btn-outline-secondary mx-1" >Editar</button>
+								<button type="button" className="btn btn-outline-danger mx-1" onClick={() => deleteTask(task.id)}>Eliminar</button>
+							</div>
+						</li>
 					))}
 				</ul>
 			</div>
